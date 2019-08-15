c43c19ee61a7562245dc83627934a4a29f7065c5
work_global <work_global@shj.com>
2019-08-15 17:06:02 +0800

    upload

--- src/views/GraduationStatistics/Index.vue
+++ src/views/GraduationStatistics/Index.vue
@@ -27,6 +27,21 @@
                 <!-- <el-form-item>
                     <el-button type="primary" @click="handleAdd">新增</el-button>
                 </el-form-item> -->
+                 <el-form-item>
+                    <el-upload
+                        class="upload"
+                        action="http://localhost:8081/api/GraduationStatistics/Import"
+                        :on-preview="handlePreview"
+                        :on-remove="handleRemove"
+                        :before-remove="beforeRemove"
+                        multiple
+                        :limit="3"
+                        :on-exceed="handleExceed"
+                        :file-list="fileList">
+                        <el-button size="small" type="primary">点击上传</el-button>
+                        <span slot="tip" class="el-upload__tip">只能上传excel文件</span>
+                    </el-upload>
+                  </el-form-item>
             </el-form>
         </el-col>
          <!--列表-->
@@ -87,6 +102,7 @@
     export default {
         data(){
             return{
+                fileList:[],
                 listLoading:false,
                 Graduations:[],
                 sels: [],//列表选中列
@@ -142,6 +158,18 @@
             }
         },
         methods:{
+            handleRemove(file, fileList) {
+                console.log(file, fileList);
+            },
+            handlePreview(file) {
+                console.log(file);
+            },
+            handleExceed(files, fileList) {
+                this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+            },
+            beforeRemove(file, fileList) {
+                return this.$confirm(`确定移除 ${ file.name }？`);
+            },
             handleCurrentChange(val) {
                 this.page = val;
                 this.getGraduations();
